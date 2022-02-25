6376fbfa6748cae6b988c9c05cc43e77a0166e95
Doha ElHady <86476979+DohaElHady@users.noreply.github.com>
2022-02-25 10:57:31 +0200

    Update GloveForDeaf_main.c

--- GloveForDeaf_MicroshipStudioProject/GloveForDeaf/GloveForDeaf_main.c
+++ GloveForDeaf_MicroshipStudioProject/GloveForDeaf/GloveForDeaf_main.c
@@ -27,19 +27,6 @@ int main(void)
 		Signs_voidTranslateSign(u8Local_ResultTemp);
 		while (u8Local_ResultTemp==Signs_u8CheckSign());
 
-		/* Code for testing sensors values*/
-		/*
-		for (int i=0;i<8;i++)
-		{
-
-			ADC_voidSetChannel(i);
-			u16Local_ResultTemp = ADC_u16ReadValue();
-			u16Local_ResultTemp = (u16Local_ResultTemp*500UL/1024UL);
-			LCD_voidWriteu8Num(i);
-			LCD_voidWriteu8Num((u8)u16Local_ResultTemp);
-			_delay_ms(200);
-			LCD_voidLCDClear();
-		}*/
 	}
 	return 0 ;
 }
